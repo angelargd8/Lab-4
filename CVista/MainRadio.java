1e234a44e8f989a2d0af2ed584d95be22868c07d
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2022-11-17 21:10:45 -0600

    creación del main

---
+++ CVista/MainRadio.java
@@ -0,0 +1,12 @@
+
+package CVista;
+public class MainRadio {
+    
+    /** 
+     * @param args
+     */
+    public static void main(String[] args) {
+        //MostrarMenu mostrarMenu= new MostrarMenu();
+        //mostrarMenu.MostrarPrograma();
+    }
+}
